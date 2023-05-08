9f1e57dfdd4bbc06200dd4a910b018dbb11ce619
aleksandr <kuzinalexandar@yandex.ru>
2023-05-08 23:18:22 +0300

    init

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from "process";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors()
+  console.log(process.env.PORT)
   app.setGlobalPrefix('/api')
-  await app.listen(4200);
+  await app.listen(process.env.PORT || 4200);
 }
 bootstrap();
