2601526204c8ad9383316df057e242bc03d31682
aleksandr <kuzinalexandar@yandex.ru>
2023-05-09 00:22:39 +0300

    vercel

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,7 @@ import {AppModule} from "./src/app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors()
-  console.log(process.env.PORT)
   app.setGlobalPrefix('/api')
-  await app.listen(process.env.PORT || 4200);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
