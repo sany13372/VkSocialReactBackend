fb62ca4fb94da54c1003313a825699cd48b403e6
aleksandr <kuzinalexandar@yandex.ru>
2023-05-15 15:47:25 +0300

    change env

--- src/config/mongoconfig.ts
+++ src/config/mongoconfig.ts
@@ -1,5 +1,6 @@
 import { TypegooseModuleOptions } from 'nestjs-typegoose'
+import * as process from "process";
 
 export const getMongoBdConfig =  async ():Promise<TypegooseModuleOptions> => ({
-    uri:'mongodb://localhost:27017'
+    uri:process.env.MONGO_URL
 })
